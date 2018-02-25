2a26412fda6f89ad23e8c165729b1f6ce030888e
User <denisenko.ia1987@gmail.com>
2018-02-26 01:22:21 +0200

    initialise

---
+++ src/common.js
@@ -0,0 +1,18 @@
+// require SCSS
+// console.log('./scss/main.scss');
+// import './scss/main.scss';
+
+
+// require polyfill
+import 'babel-polyfill';
+
+
+// require scripts
+
+
+// require app component
+
+
+
+console.log('test');
+ [[[]]]
